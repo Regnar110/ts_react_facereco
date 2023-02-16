70c0b5aaa20f793111dfe6446da107900e9bcc47
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-02-16 18:12:11 +0100

    deploying on netlify

--- src/utils/autoFetch.ts
+++ src/utils/autoFetch.ts
@@ -1,6 +1,6 @@
 
 export const autoFetch = async <T, U>(path:string, method:string, reqBody: T):Promise<U> => {
-    const response = await fetch(`http://localhost:3001/${path}`, {
+    const response = await fetch(`https://smartbrain-377721.lm.r.appspot.com/${path}`, {
         method: method,
         headers: {
             "Content-Type": "application/json"
