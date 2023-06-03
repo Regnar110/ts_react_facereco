c2798db5252ebcb9cd548ff0f541361947705d16
Mateusz <66961646+Regnar110@users.noreply.github.com>
2023-06-04 00:24:44 +0200

    added headers - fixing cors policy bug

--- src/utils/autoFetch.ts
+++ src/utils/autoFetch.ts
@@ -2,9 +2,15 @@
 export const autoFetch = async <T, U>(path:string, method:string, reqBody: T):Promise<U> => {
     const response = await fetch(`https://smartbrain-377721.lm.r.appspot.com/${path}`, {
         method: method,
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
         headers: {
-            "Content-Type": "application/json"
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(reqBody)
     })
     const data:U = await response.json();
